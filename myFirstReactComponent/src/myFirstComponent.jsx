02e1119d11055c81cb77206f77b499ba44680c2e
LaithsPage <jacob.riley419@gmail.com>
2023-11-05 17:51:46 -0500

    updated my first component to reflect further learning

--- myFirstReactComponent/src/myFirstComponent.jsx
+++ myFirstReactComponent/src/myFirstComponent.jsx
@@ -1,5 +1,35 @@
+function ListItem(props) {
+    return <li>{props.animal}</li>
+}
+
+function List(props) {
+
+    if(!props.animals) {
+        return <div>Loading...</div>;
+    }
+
+    if (props.animals.length === 0) {
+        return <div>There are no animals in the list!</div>;
+    }
+    
+    return (
+        <ul>
+        {props.animals.map((animal) => {
+            return animal.startsWith("B") ? <ListItem key={animal} animal={animal} /> : null;
+        })}
+    </ul>
+    );
+}
+
 function Buddy() {
-    return <h1>&quot;Meow!!!&quot;</h1>
+    const animals = ["Buddy", "Laith", "Quinn", "Becca"];
+
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <List animals={animals} />
+        </div>
+    );
 }
 
 export default Buddy;
