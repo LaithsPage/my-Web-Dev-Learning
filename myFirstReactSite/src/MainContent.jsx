c9bcc23ecbf149fe2e5a3aa5b6a4f6f389093337
LaithsPage <jacob.riley419@gmail.com>
2023-11-18 13:14:12 -0500

    setting up the grid layout on main page and refactoring

--- myFirstReactSite/src/MainContent.jsx
+++ myFirstReactSite/src/MainContent.jsx
@@ -38,8 +38,8 @@ For now, a grid is needed that has 2 columns, within the columns are
 // can break array up into one array for each column so it is easier to differentiate between and change later
 const imagePaths = [
     { id: 'img1', path: '../public/images/easternmarket_composite_05.jpg' },
-    { id: 'img2', path: '../public/images/westview_composite_07.jpg' },
-    // { id: 'img2', path: '../public/images/westview 2.jpg' },
+   // { id: 'img2', path: '../public/images/westview_composite_07.jpg' },
+    { id: 'img2', path: '../public/images/westview 2.jpg' },
     { id: 'img3', path: '../public/images/rochester_composite_09.jpg' },
     { id: 'img4', path: '../public/images/westview_composite_19.jpg' },
     { id: 'img5', path: '../public/images/easternmarket_composite_05.jpg' },
@@ -58,13 +58,11 @@ export default function MainContent() {
             
             <div className="main-grid">
                 <div className="column-1">
-                    <div>
                         {imagePaths.slice(0, 2).map(({id, path}) => (
                             <div key={id}>
                                 <img src={path} alt={`Image ${id}`} />
                             </div>
                         ))}
-                    </div>
                 </div>
 
                 <div className="column-2">
@@ -73,25 +71,27 @@ export default function MainContent() {
                     ))}
                 </div>
 
-                <div className="column-3">
+{/*                 <div className="column-3">
                     <img src={imagePaths[5].path} alt={`Image ${imagePaths[5].id}`} />
-                </div>
-                
-                <div>
-                    <p>Something about Santa Riley here. Something about Santa Riley here. Something about Santa Riley here. Something about Santa Riley here. </p>
-                    <a href="#">Santa Link here </a>
-                </div>
-
-                <div className="column-4">
-                    <img src={imagePaths[6].path} alt={`Image ${imagePaths[6].id}`} />
-                    <h3>Headline Text</h3>
-                    <p>Follow up info</p>
-                    <form>
-                        <label for="fname">First name:</label><br />
-                        <input type="text" id="fname" name="fname" /><br />
-                    </form>
-                </div>
+                </div> */}
+            
             </div>
+
+            <div>
+                <p>Something about Santa Riley here. Something about Santa Riley here. Something about Santa Riley here. Something about Santa Riley here. </p>
+                <a href="#">Santa Link here </a>
+            </div>
+
+            <div className="column-4">
+                <img src={imagePaths[6].path} alt={`Image ${imagePaths[6].id}`} />
+                <h3>Headline Text</h3>
+                <p>Follow up info</p>
+                <form>
+                    <label for="fname">First name:</label><br />
+                    <input type="text" id="fname" name="fname" /><br />
+                </form>
+            </div>
+
         </div>
         </>
     );
